678461a6dbe8f5b20f5ccd98ac072d0288a60d1c
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-01-14 09:41:59 +0530

    little fixes

---
+++ Mini-Project/js/validate.js
@@ -0,0 +1,24 @@
+function validateName() {
+
+
+}
+
+function validateCIN() {
+
+    
+}
+
+function validateEmail() {
+
+    
+}
+
+function validatePassword() {
+
+    
+}
+
+function validateConfirmPwd() {
+
+    
+}
